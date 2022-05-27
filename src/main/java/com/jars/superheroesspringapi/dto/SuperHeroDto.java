bb1f2648a975d93d5626912c68926aa3366c59b1
JarssS8 <adriancgs@gmail.com>
2022-05-27 02:10:22 +0200

    Solve small swagger issue

--- src/main/java/com/jars/superheroesspringapi/dto/SuperHeroDto.java
+++ src/main/java/com/jars/superheroesspringapi/dto/SuperHeroDto.java
@@ -1,8 +1,10 @@
 package com.jars.superheroesspringapi.dto;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class SuperHeroDto {
     private long id;
     private String name;
